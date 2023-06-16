719be9e6d4bb06028792425f7766211e1ae47983
Oscar Rivera <orivera@applaudostudios.com>
2023-06-16 13:37:54 -0600

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import useApiHook from "./hook/useApiHook";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const App: React.FC = () => {
+  const { data, loading, error } = useApiHook<Post[]>(
+    "https://jsonplaceholder.typicode.com/postss", //endpoint url
+    { timeout: 5000 } //extra parameter just for example
   );
-}
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (data) {
+    return (
+      <div>
+        {data.map((post) => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+};
 
 export default App;
